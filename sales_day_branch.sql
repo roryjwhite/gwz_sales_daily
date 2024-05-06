5f7c8eeed5764f23202fe8aef3be802755cf4cd6
LiamLA2 <liam199838@live.co.uk>
2024-05-06 15:13:13 +0100

    Update sales_day_branch.sql
    created margin

--- sales_day_branch.sql
+++ sales_day_branch.sql
@@ -1,6 +1,5 @@
 SELECT date_date
-    , ROUND(SUM(turnover),2) AS turnover
-    , ROUND(SUM(purchase_cost),2) AS purchase_cost
+    , ROUND(SUM(turnover - purchase_cost),2) AS margin
 FROM skillful-cortex-420410.course14.gwz_sales
 GROUP BY date_date
-ORDER BY date_date
+ORDER BY date_date;
