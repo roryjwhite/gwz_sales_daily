4fa06c96eedbf2b3407174b25bf00b16b968bfc1
roryjwhite <rjwhite95@icloud.com>
2024-05-06 15:10:52 +0100

    Update sales_day_branch.sql
    discount

--- sales_day_branch.sql
+++ sales_day_branch.sql
@@ -1,6 +1,6 @@
 SELECT date_date
     , ROUND(SUM(turnover),2) AS turnover
-    , ROUND(SUM(purchase_cost),2) AS purchase_cost
+    , ROUND(SUM(turnover_before_promo) - SUM(turnover),2) AS discount
 FROM skillful-cortex-420410.course14.gwz_sales
 GROUP BY date_date
 ORDER BY date_date
